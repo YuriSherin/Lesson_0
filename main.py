39a69ab70c6499b79e0e7c7b970507749b06ba9d
YuriSherin <YuriSherin@mail.ru>
2024-08-08 20:41:07 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+print()
+
+# 2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+print()
+
+# "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+print()
+
+# "4th program"
+str = '123.456'
+num = float('123.456')
+num = int(num * 10)
+print((((num % 1000) % 100) % 10))
